e3617afb46fad42131b5fe72df2cc11bb5882260
hamedghd <h.gharibdoost@gmail.com>
2021-06-07 08:53:07 +0200

    Add comments.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+// imports the http, fs and url modules
 const http = require('http'),
   fs = require('fs'),
   url = require('url');
 
+// Creates a new server.
 http.createServer((request, response) => {
   let addr = request.url,
     q = url.parse(addr, true),
     filePath = '';
 
+  // Creates a log of recent requests made to the server.
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' +
   new Date() + '\n\n', (err) => {
     if (err) {
@@ -16,12 +19,17 @@ http.createServer((request, response) => {
     }
   });
 
+  // If pathname includes documentation,
+  // it returns the documentation.html.
+  // If pathname doesn't include “documentation”,
+  // the if-else statement returns “index.html”.
   if (q.pathname.includes('documentation')) {
     filePath = (__dirname + '/documentation.html');
   } else {
     filePath = 'index.html';
   }
-
+  // The fs module sends back the appropriate file.
+  // Throws an error and terminates the function if the file can’t be read.
   fs.readFile(filePath, (err, data) => {
     if (err) {
       throw err;
@@ -33,4 +41,5 @@ http.createServer((request, response) => {
 
   })
 }).listen(8080);
+// Creates a message on console.
 console.log('My test server is running on Port 8080.');
