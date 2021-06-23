26bd8ef969d7d6f68b1a4fc1a26aad2ff91d7819
hamedghd <h.gharibdoost@gmail.com>
2021-06-23 08:04:26 +0200

    Define movie's schema

--- models.js
+++ models.js
@@ -1,2 +1,20 @@
 // Imports mongoose package.
 const mongoose = require('mongoose');
+// Defines a schema for documents in the "Movies" collection
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    Birth: Date,
+    Death: Date
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
+});
