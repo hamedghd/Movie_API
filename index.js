0829a502524d9c73c71519b71d259c184cd4cb3c
hamedghd <h.gharibdoost@gmail.com>
2021-06-23 10:58:01 +0200

    Allow existing users to deregister

--- index.js
+++ index.js
@@ -186,3 +186,19 @@ app.delete('/users/:Username/Movies/:MovieID', (req, res) => {
     }
   });
 });
+
+// Allows existing users to deregister.
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
