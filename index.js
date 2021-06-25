443fd246846520d6b8c6cda28650e192c27fb36b
hamedghd <h.gharibdoost@gmail.com>
2021-06-25 10:54:16 +0200

    Apply JWT authentication to /movies endpoint

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', function (req, res) {
 });
 
 // Maps a route at the endpoint “/movies”.
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
