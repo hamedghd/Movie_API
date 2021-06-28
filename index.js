481d3fc76eb14883a8f016a929ce48e1469992ca
hamedghd <h.gharibdoost@gmail.com>
2021-06-28 10:14:43 +0200

    Restric the access to the API

--- index.js
+++ index.js
@@ -45,7 +45,20 @@ app.get('/', function (req, res) {
 
 // Imports CORS
 const cors = require('cors');
-app.use(cors());
+// app.use(cors());
+// To restrict the access to the API from different domains:
+let allowedOrigins = ['http://localhost:8080'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Maps a route at the endpoint “/movies”.
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
