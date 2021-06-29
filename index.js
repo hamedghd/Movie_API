3f164791d3e60897f243c850c0be55eec25773ad
hamedghd <h.gharibdoost@gmail.com>
2021-06-29 10:23:25 +0200

    Allow all the domains to request to the API

--- index.js
+++ index.js
@@ -46,20 +46,20 @@ app.get('/', function (req, res) {
 
 // Imports CORS
 const cors = require('cors');
-// app.use(cors());
+app.use(cors());
 // To restrict the access to the API from different domains:
-let allowedOrigins = ['http://localhost:8080'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// let allowedOrigins = ['http://localhost:8080'];
+//
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // Imports Express-validator
 const { check, validationResult } = require('express-validator');
