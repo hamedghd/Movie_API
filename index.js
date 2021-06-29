51397c6691fb4f03b32d0f5f458e4aac166918cf
hamedghd <h.gharibdoost@gmail.com>
2021-06-29 15:20:38 +0200

    Limit the domains that have access to the API

--- index.js
+++ index.js
@@ -12,20 +12,21 @@ const express = require('express'),
     cors = require('cors'),
     { check, validationResult } = require('express-validator');
 
-app.use(cors());
+//app.use(cors());
 // To restrict the access to the API from different domains:
-// let allowedOrigins = ['http://localhost:8080'];
-//
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+let allowedOrigins = ['http://localhost:8080', 'https://myflix-movieapi.herokuapp.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(bodyParser.json());
 // Serve documentation.html (static files) from '/public' folder.
 app.use(express.static('public'));
